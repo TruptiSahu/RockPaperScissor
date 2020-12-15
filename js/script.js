27e550ebd65aa5554d5b92c87576fe631d937f70
TruptiSahu <truptiranjansahu2@gmail.com>
2020-12-14 23:45:17 -0800

    Comment Add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//* Selecting element from DOM
 const playerBtns = document.querySelectorAll('.player-btn');
 const gameRound = document.querySelector('.game__round');
 const handImg = document.querySelectorAll('.hand-img');
